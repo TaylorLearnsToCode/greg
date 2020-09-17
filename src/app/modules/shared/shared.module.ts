90345c0e8b30abbd6f84cae1c16c3754831ed9f8
TaylorLearnsToCode <clericswearemail@gmail.com>
2020-09-17 08:15:10 -0400

    Update documentation

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -4,8 +4,10 @@ import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { PageTemplateComponent } from './templates/page-template/page-template.component';
 
+/** "Private" container for components in the Shared Module to reduce file length, re-using the variable as a spread */
 const components = [PageTemplateComponent, WelcomeComponent, MenuBarComponent];
 
+/** Container module for common or shared widgets and templates for the GREG app */
 @NgModule({
   declarations: [...components],
   imports: [CommonModule],
