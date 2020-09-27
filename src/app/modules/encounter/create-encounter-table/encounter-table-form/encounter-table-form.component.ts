6be41ce1ce237b566b73577bbf8ef72f82495b20
TaylorLearnsToCode <clericswearemail@gmail.com>
2020-09-27 06:08:38 -0400

    Added result table; added rolling dice util

--- src/app/modules/encounter/create-encounter-table/encounter-table-form/encounter-table-form.component.ts
+++ src/app/modules/encounter/create-encounter-table/encounter-table-form/encounter-table-form.component.ts
@@ -1,15 +1,21 @@
 import {
   Component,
+  EventEmitter,
   Input,
   OnChanges,
   OnInit,
+  Output,
   SimpleChanges,
 } from '@angular/core';
 import { FormArray, FormGroup } from '@angular/forms';
 import { DiceRolled } from '@shared/model/dice-rolled.model';
 import { areEqual, doesExist } from '@shared/utilities/common-util/common.util';
 import { buildFormFromObject } from '@shared/utilities/form-util/form.util';
-import { EncounterTable } from '../model/encounter-table.model';
+import {
+  EncounterTable,
+  EncounterTableActions,
+  IEncounterTableAction,
+} from '../model/encounter-table.model';
 
 @Component({
   selector: 'greg-encounter-table-form',
@@ -18,22 +24,24 @@ import { EncounterTable } from '../model/encounter-table.model';
 })
 export class EncounterTableFormComponent implements OnInit, OnChanges {
   @Input() encounterTable: EncounterTable;
+  @Output() encounterTableAction = new EventEmitter<IEncounterTableAction>();
 
   encounterTableForm: FormGroup = buildFormFromObject(
     new EncounterTable()
   ) as FormGroup;
-
   get formDiceRolled(): FormArray {
     return this.encounterTableForm.get('diceRolled') as FormArray;
+  }
+  get formEncounters(): FormArray {
+    return this.encounterTableForm.get('encounters') as FormArray;
   }
 
   constructor() {}
 
-  ngOnInit(): void {
-    // console.log(this.encounterTableForm);
-  }
+  ngOnInit(): void {}
 
   ngOnChanges(changes: SimpleChanges): void {
+    console.log(this.encounterTableForm);
     if (changes.encounterTable) {
       if (
         !areEqual(
@@ -50,10 +58,29 @@ export class EncounterTableFormComponent implements OnInit, OnChanges {
     this.formDiceRolled.push(buildFormFromObject(new DiceRolled(1, 6)));
   }
 
-  removeDieRolled(idx?: number): void {
-    if (this.formDiceRolled.length > 0) {
-      idx = doesExist(idx) ? idx : this.formDiceRolled.length - 1;
+  clearDiceRolled(): void {
+    while (this.formDiceRolled.length > 0) {
+      this.formDiceRolled.removeAt(0);
+    }
+  }
+
+  removeDieRolled(idx: number): void {
+    if (this.validateDieRemove(idx)) {
       this.formDiceRolled.removeAt(idx);
     }
+  }
+
+  updateTable(): void {
+    this.encounterTableAction.emit({
+      action: EncounterTableActions.UPDATE_DICE_ROLLED,
+      payload: this.formDiceRolled.value,
+    } as IEncounterTableAction);
+  }
+
+  private validateDieRemove(idx: number): boolean {
+    if (!doesExist(idx)) {
+      throw new Error('Index Number is Required');
+    }
+    return this.formDiceRolled.length > 0;
   }
 }
---
+++ src/app/modules/shared/model/save-as.model.ts
@@ -0,0 +1,4 @@
+export class SaveAs {
+  class: string;
+  level: number;
+}
