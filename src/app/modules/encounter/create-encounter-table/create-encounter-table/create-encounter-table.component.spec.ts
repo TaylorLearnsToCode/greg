90641a1aa60fd2d8010f058742a25e9ea81ecee0
TaylorLearnsToCode <clericswearemail@gmail.com>
2020-09-29 21:06:03 -0400

    Unit tests, round two

--- src/app/modules/encounter/create-encounter-table/create-encounter-table/create-encounter-table.component.spec.ts
+++ src/app/modules/encounter/create-encounter-table/create-encounter-table/create-encounter-table.component.spec.ts
@@ -1,14 +1,32 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  EncounterTableActions,
+  IEncounterTableAction,
+} from '../model/encounter-table.model';
+import { Encounter } from '../model/encounter.model';
+import { CreateEncounterFacadeService } from '../services/create-encounter-facade/create-encounter-facade.service';
 import { CreateEncounterTableComponent } from './create-encounter-table.component';
 
 describe('CreateEncounterTableComponent', () => {
   let component: CreateEncounterTableComponent;
   let fixture: ComponentFixture<CreateEncounterTableComponent>;
 
+  const facadeSpy = jasmine.createSpyObj('CreateEncounterFacadeService', [
+    'destroy',
+    'initialize',
+    'handleEncounterTableAction',
+  ]);
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [CreateEncounterTableComponent],
+      providers: [
+        {
+          provide: CreateEncounterFacadeService,
+          useValue: facadeSpy,
+        },
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
@@ -19,7 +37,33 @@ describe('CreateEncounterTableComponent', () => {
     fixture.detectChanges();
   });
 
+  afterEach(() => resetAllCalls());
+
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(facadeSpy.initialize).toHaveBeenCalled();
   });
+
+  it('should destroy the facade', () => {
+    component.ngOnDestroy();
+    expect(facadeSpy.destroy).toHaveBeenCalled();
+  });
+
+  it('should delegate encounter table actions', () => {
+    const action = {
+      action: EncounterTableActions.UPDATE_ENCOUNTERS,
+      payload: [new Encounter(1)],
+    } as IEncounterTableAction;
+    component.onEncounterTableAction(action);
+    expect(facadeSpy.handleEncounterTableAction).toHaveBeenCalledWith(action);
+  });
+
+  /* Utility Functions */
+  function resetAllCalls(): void {
+    Object.keys(facadeSpy).forEach((key) => {
+      if (facadeSpy[key].calls) {
+        facadeSpy[key].calls.reset();
+      }
+    });
+  }
 });
