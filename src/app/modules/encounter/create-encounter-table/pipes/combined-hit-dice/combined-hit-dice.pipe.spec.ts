d0f00cb0520d2428567e89206397721d8c6a83f1
TaylorLearnsToCode <wtbeeson@gmail.com>
2020-10-13 10:29:56 -0400

    Test file for combined hit dice pipe

--- src/app/modules/encounter/create-encounter-table/pipes/combined-hit-dice/combined-hit-dice.pipe.spec.ts
+++ src/app/modules/encounter/create-encounter-table/pipes/combined-hit-dice/combined-hit-dice.pipe.spec.ts
@@ -1,8 +1,33 @@
+import { TestBed } from '@angular/core/testing';
+import { Monster } from '@shared/model/monster.model';
 import { CombinedHitDicePipe } from './combined-hit-dice.pipe';
 
 describe('CombinedHitDicePipe', () => {
+  let pipe: CombinedHitDicePipe;
+  let monster: Monster;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [CombinedHitDicePipe],
+    });
+    pipe = TestBed.inject(CombinedHitDicePipe);
+    monster = new Monster();
+  });
+
   it('create an instance', () => {
-    const pipe = new CombinedHitDicePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return hit dice only', () =>
+    expect(pipe.transform(monster)).toEqual('0'));
+
+  it('should return hit dice plus one', () => {
+    monster.hitPointModifier = 1;
+    expect(pipe.transform(monster)).toEqual('0+1');
+  });
+
+  it('should return hit dice minus one', () => {
+    monster.hitPointModifier = -1;
+    expect(pipe.transform(monster)).toEqual('0-1');
   });
 });
