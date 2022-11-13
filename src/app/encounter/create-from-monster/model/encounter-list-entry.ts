931963b0724728fbcae4a81d6c7bd90b68fefb3b
TaylorLearnsToCode <wtbeeson@gmail.com>
2022-11-13 14:08:29 -0500

    feat (encounter) EncounterList model

--- src/app/encounter/create-from-monster/model/encounter-list-entry.ts
+++ src/app/encounter/create-from-monster/model/encounter-list-entry.ts
@@ -1,7 +1,13 @@
 import { BoundedRange } from '@shared/model/bounded-range.model';
+import { DiceRolled } from '@shared/model/dice-rolled.model';
 import { WwwMonster } from '@shared/model/www-monster.model';
 
 export class EncounterListEntry {
   range: BoundedRange;
   encounter: WwwMonster;
+}
+
+export class EncounterList {
+  diceToRoll: DiceRolled;
+  encounters: EncounterListEntry[];
 }
