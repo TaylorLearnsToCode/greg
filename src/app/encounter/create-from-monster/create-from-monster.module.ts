6fc892fcdcad21b703723f43849e253ec169707e
TaylorLearnsToCode <clericswearemail@gmail.com>
2022-11-13 06:24:06 -0500

    feat (encounter) swap "encounter list" for "roll encounter"

--- src/app/encounter/create-from-monster/create-from-monster.module.ts
+++ src/app/encounter/create-from-monster/create-from-monster.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '@shared/shared.module';
 import { CreateFromMonsterComponent } from './components/create-from-monster/create-from-monster.component';
 import { MonsterListComponent } from './components/monster-list/monster-list.component';
-import { EncounterListComponent } from './components/encounter-list/encounter-list.component';
+import { RollEncounterComponent } from './components/roll-encounter/roll-encounter.component';
 
 const routes: Routes = [
   {
@@ -14,7 +14,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [CreateFromMonsterComponent, MonsterListComponent, EncounterListComponent],
+  declarations: [CreateFromMonsterComponent, MonsterListComponent, RollEncounterComponent],
   exports: [RouterModule],
   imports: [CommonModule, RouterModule.forChild(routes), SharedModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
