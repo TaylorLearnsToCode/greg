e941684cfd2e0b1c6db5cfdcb097e4d7cc11a323
TaylorLearnsToCode <wtbeeson@gmail.com>
2022-11-14 21:24:36 -0500

    feat (treasure) import/export

--- src/app/treasure/enter-treasure/components/treasure-list/treasure-list.component.ts
+++ src/app/treasure/enter-treasure/components/treasure-list/treasure-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { TreasureList } from '@treasure/enter-treasure/model/treasure-list.model';
 import { EnterTreasureControllerService } from '@treasure/enter-treasure/services/enter-treasure-controller/enter-treasure-controller.service';
 import { Observable } from 'rxjs';
@@ -9,11 +9,27 @@ import { Observable } from 'rxjs';
   styleUrls: ['./treasure-list.component.scss'],
 })
 export class TreasureListComponent implements OnInit {
+  @ViewChild('listImport')
+  listImportElementRef: ElementRef;
+
   treasureList$: Observable<TreasureList>;
+
+  private get listImportInput(): HTMLInputElement {
+    return this.listImportElementRef.nativeElement as HTMLInputElement;
+  }
 
   constructor(private controllerService: EnterTreasureControllerService) {}
 
   ngOnInit(): void {
     this.treasureList$ = this.controllerService.treasureList$;
+  }
+
+  exportList(): void {
+    this.controllerService.exportList();
+  }
+
+  importSavedList(): void {
+    this.controllerService.importSavedList(this.listImportInput.files[0]);
+    this.listImportInput.value = '';
   }
 }
