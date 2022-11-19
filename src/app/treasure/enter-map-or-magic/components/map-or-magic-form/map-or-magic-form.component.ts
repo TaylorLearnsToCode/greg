6f2e1d8087cf73db94b57f244e3a9ae91d113dc9
TaylorLearnsToCode <wtbeeson@gmail.com>
2022-11-18 21:26:27 -0500

    feat (treasure) List Display and Builder

--- src/app/treasure/enter-map-or-magic/components/map-or-magic-form/map-or-magic-form.component.ts
+++ src/app/treasure/enter-map-or-magic/components/map-or-magic-form/map-or-magic-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, UntypedFormGroup } from '@angular/forms';
+import { buildFormFromObject } from '@shared/utilities/form-util/form.util';
+import { MapOrMagicControllerServiceService } from '@treasure/enter-map-or-magic/services/map-or-magic-controller-service/map-or-magic-controller-service.service';
+import { MagicItem } from '@treasure/treasure-common/model/magic-item.model';
 
 @Component({
   selector: 'greg-map-or-magic-form',
@@ -6,7 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map-or-magic-form.component.scss'],
 })
 export class MapOrMagicFormComponent implements OnInit {
-  constructor() {}
+  mapOrMagicEntryForm: UntypedFormGroup;
 
-  ngOnInit(): void {}
+  constructor(private controllerService: MapOrMagicControllerServiceService) {}
+
+  ngOnInit(): void {
+    this.initializeMapOrMagicEntryForm();
+  }
+
+  addToList(): void {
+    this.controllerService.addToList(this.mapOrMagicEntryForm.value);
+    this.initializeMapOrMagicEntryForm();
+  }
+
+  private initializeMapOrMagicEntryForm(): void {
+    this.mapOrMagicEntryForm = buildFormFromObject(
+      new MagicItem()
+    ) as FormGroup;
+  }
 }
