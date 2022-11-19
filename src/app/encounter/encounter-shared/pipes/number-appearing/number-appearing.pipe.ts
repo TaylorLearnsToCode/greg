226b93e0edb3fda5c6aa46805965080269fdfb6c
TaylorLearnsToCode <wtbeeson@gmail.com>
2022-11-19 12:49:28 -0500

    feat (treasure) pretty up magic item

--- src/app/encounter/encounter-shared/pipes/number-appearing/number-appearing.pipe.ts
+++ src/app/encounter/encounter-shared/pipes/number-appearing/number-appearing.pipe.ts
@@ -1,14 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { EncounterLocation } from '@encounter/encounter-shared/model/encounter-locationS.enum';
 import { Monster } from '@shared/model/monster.model';
-import { BoundedRangePipe } from '@shared/pipes/bounded-range/bounded-range.pipe';
+import { DiceRangePipe } from '@shared/pipes/dice-range/dice-range.pipe';
 
 @Pipe({
   name: 'numberAppearing',
 })
 export class NumberAppearingPipe
-  extends BoundedRangePipe
-  implements PipeTransform {
+  extends DiceRangePipe
+  implements PipeTransform
+{
   transform(monster: Monster, encounterLocation: EncounterLocation): string {
     return super.transform(
       encounterLocation == EncounterLocation.WILDERNESS
