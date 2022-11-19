fd06a8b10dbacdec6ce83203cc331b88ca76f99e
TaylorLearnsToCode <clericswearemail@gmail.com>
2022-11-18 21:04:41 -0500

    feat (treasure) more refactor; add list name input

--- src/app/treasure/enter-treasure/components/treasure-form/treasure-form.component.ts
+++ src/app/treasure/enter-treasure/components/treasure-form/treasure-form.component.ts
@@ -11,7 +11,7 @@ import {
   doesExist,
 } from '@shared/utilities/common-util/common.util';
 import { buildFormFromObject } from '@shared/utilities/form-util/form.util';
-import { MagicItem } from '@treasure/enter-treasure/model/magic-item.model';
+import { MagicItem } from '@treasure/treasure-common/model/magic-item.model';
 import {
   GemOrJewel,
   TreasureListEntry,
