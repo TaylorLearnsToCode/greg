6286563a2778501b8fcb05c1b2a556bf95f3bb82
TaylorLearnsToCode <clericswearemail@gmail.com>
2022-11-27 11:44:18 -0500

    feat (treasure) remove for gems and jewels

--- src/app/treasure/enter-treasure/model/treasure-list.model.ts
+++ src/app/treasure/enter-treasure/model/treasure-list.model.ts
@@ -2,6 +2,8 @@ import { DiceRolled } from '@shared/model/dice-rolled.model';
 import { TreasureListEntry } from './treasure-list-entry.model';
 
 export class TreasureList {
-  diceToRoll: DiceRolled = new DiceRolled();
+  diceToRoll: DiceRolled = new DiceRolled({
+    pips: 100,
+  } as DiceRolled);
   entries: TreasureListEntry[] = [];
 }
