cdd1727e001276d3b7913121df7a2b754c741356
TaylorLearnsToCode <clericswearemail@gmail.com>
2022-11-28 22:09:16 -0500

    feat (treasure) implement roll gems

--- src/app/treasure/enter-treasure/model/treasure-list-entry.model.ts
+++ src/app/treasure/enter-treasure/model/treasure-list-entry.model.ts
@@ -21,18 +21,40 @@ export class TreasureRollResult {
 }
 
 export class GemRollResult {
-  no10: number = 0;
-  no50: number = 0;
-  no100: number = 0;
-  no500: number = 0;
-  no1k: number = 0;
-  no5k: number = 0;
-  no10k: number = 0;
-  no25k: number = 0;
-  no50k: number = 0;
-  no100k: number = 0;
-  no500k: number = 0;
+  n10: number = 0;
+  n50: number = 0;
+  n100: number = 0;
+  n500: number = 0;
+  n1000: number = 0;
+  n5000: number = 0;
+  n10000: number = 0;
+  n25000: number = 0;
+  n50000: number = 0;
+  n100000: number = 0;
+  n500000: number = 0;
 }
+
+export const RolledGemValue: Map<number, string> = new Map([
+  [1, 'n10'],
+  [2, 'n50'],
+  [3, 'n100'],
+  [4, 'n500'],
+  [5, 'n1000'],
+  [6, 'n5000'],
+  [7, 'n10000'],
+  [8, 'n25000'],
+  [9, 'n50000'],
+  [10, 'n100000'],
+  [11, 'n500000'],
+]);
+
+export const RolledGemChances: Map<number, number> = new Map([
+  [1, 10],
+  [2, 25],
+  [3, 75],
+  [4, 90],
+  [5, 100],
+]);
 
 export class MapsAndMagicEntry {
   name: string = '';
