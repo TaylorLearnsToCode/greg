7661a92c7e9431071f854567c3761e28d27f3993
TaylorLearnsToCode <wtbeeson@gmail.com>
2023-04-11 17:32:54 -0400

    feat (treasure map) progress point, treasure article

--- src/app/shared/model/dao/data-state.model.ts
+++ src/app/shared/model/dao/data-state.model.ts
@@ -1,5 +1,5 @@
-import { constructInstance } from '@shared/utilities/common-util/common.util';
 import { ReferenceEntryTable } from '@shared/model/framework/reference-entry-table.model';
+import { constructInstance } from '@shared/utilities/common-util/common.util';
 import { MagicItem } from '../treasure/magic-item.model';
 import { TreasureArticle } from '../treasure/treasure-article.model';
 import { TreasureType } from '../treasure/treasure-type.model';
@@ -8,6 +8,7 @@ export class DataState {
   magicItems: MagicItem[] = [];
   magicItemTables: ReferenceEntryTable[] = [];
   treasureArticles: TreasureArticle[] = [];
+  treasureMapRefs: TreasureArticle[] = [];
   treasureTypes: TreasureType[] = [];
 
   constructor(state?: any) {
