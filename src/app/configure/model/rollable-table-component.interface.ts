5da0655741b4068efdaf4af7e3e4b86d587f7fdd
TaylorLearnsToCode <clericswearemail@gmail.com>
2023-04-15 17:15:44 -0400

    feat (treasure map) save map

--- src/app/configure/model/rollable-table-component.interface.ts
+++ src/app/configure/model/rollable-table-component.interface.ts
@@ -1,5 +1,8 @@
 /** Implement this interface if the component is designed for use with the RollableTableTemplate */
 export interface RollableTableComponent {
+  /** Supported persistence type to be imported/exported by the table */
+  PERSISTENCE_TYPE: string;
+
   /** Handler method for when the rollable table template emits an import event */
   handleImport(event: any): void;
 }
