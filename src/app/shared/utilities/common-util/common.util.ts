9eef108eb6942ba91f9d0d01d491f775143548e8
TaylorLearnsToCode <wtbeeson@gmail.com>
2023-04-20 17:38:09 -0400

    feat (treasure map) filter treasure articles by name
    POC solution. Known issue - it doesn't work until you touch the form.

--- src/app/shared/utilities/common-util/common.util.ts
+++ src/app/shared/utilities/common-util/common.util.ts
@@ -80,6 +80,23 @@ export function insertOrReplace<T>(
 }
 
 /**
+ * Returns whether or not the provided object is defined, but empty.
+ * * An empty array is defined as an array with no entries
+ * * An empty string is defined as a string with no characters
+ *
+ * @param  {string|any[]} obj
+ */
+export function isEmpty(obj: string | any[]): boolean {
+  if (Array.isArray(obj)) {
+    return isArrayEmpty(obj);
+  } else if (typeof obj === 'object') {
+    return Object.keys(obj).length === 0;
+  } else {
+    return isStringEmpty(obj);
+  }
+}
+
+/**
  * Removes the target item from the target collection based on the provided identifier.
  * Comma-delimited identifers are supported if multiple fields are to be checked.
  *
@@ -134,4 +151,24 @@ function findIndexMatchingAllKeys<T>(
     }
   }
   return index;
+}
+
+/**
+ * Returns TRUE if an array a is neither NULL nor UNDEFINED but contains
+ * 0 entries within it.
+ *
+ * @param  {any[]} a
+ */
+function isArrayEmpty(a: any[]): boolean {
+  return doesExist(a) && a.length === 0;
+}
+
+/**
+ * Returns TRUE if a string s is neither NULL nor UNDEFINED but does
+ * have a length of 0.
+ *
+ * @param  {string} s
+ */
+function isStringEmpty(s: string): boolean {
+  return doesExist(s) && s.length === 0;
 }
