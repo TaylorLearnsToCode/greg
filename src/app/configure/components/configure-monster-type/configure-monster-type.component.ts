f631a33a205737c59f5c296399d2c858df1eb80b
TaylorLearnsToCode <clericswearemail@gmail.com>
2023-04-28 05:45:26 -0400

    feat (configure monster) treasure type as selection

--- src/app/configure/components/configure-monster-type/configure-monster-type.component.ts
+++ src/app/configure/components/configure-monster-type/configure-monster-type.component.ts
@@ -3,7 +3,10 @@ import { FormGroup } from '@angular/forms';
 import { PERSISTENCE_TYPES } from '@assets/persistence-types.config';
 import { QuantifiableItemComponent } from '@configure/model/quantifiable-item-component.interface';
 import { MonsterType } from '@shared/model/monster/monster-type.model';
+import { TreasureType } from '@shared/model/treasure/treasure-type.model';
+import { DataManagerService } from '@shared/services/data-manager/data-manager.service';
 import { buildFormFromObject } from '@shared/utilities/form-util/form.util';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'greg-configure-monster-type',
@@ -16,10 +19,14 @@ export class ConfigureMonsterTypeComponent
   readonly PERSISTENCE_TYPE = PERSISTENCE_TYPES.monsterType;
 
   monsterTypeForm: FormGroup;
+  treasureType$: Observable<TreasureType[]>;
 
-  constructor() {}
+  constructor(private dataService: DataManagerService) {}
 
   ngOnInit(): void {
+    this.treasureType$ = this.dataService.dataState$.pipe(
+      map((state) => state.treasureTypes)
+    );
     this.resetForm();
   }
 
