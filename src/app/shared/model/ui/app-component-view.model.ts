f2df911da85683391e439b03cb5848e78aca35f3
TaylorLearnsToCode <clericswearemail@gmail.com>
2023-04-29 06:04:11 -0400

    feat (configure monster) layout updates

--- src/app/shared/model/ui/app-component-view.model.ts
+++ src/app/shared/model/ui/app-component-view.model.ts
@@ -1,9 +1,10 @@
-import { constructInstance } from '@shared/utilities/common-util/common.util';
 import { NavRoute } from '@shared/model/ui/nav-route.interface';
+import { constructInstance } from '@shared/utilities/common-util/common.util';
 
 export class AppComponentViewModel {
   headerSubtext: string = '';
   headerText: string = 'GREG - Guided Random Encounter Generator';
+  mainStyles: object = { 'max-height': '100vh' };
   navRoutes: NavRoute[] = [];
 
   constructor(state?: any) {
