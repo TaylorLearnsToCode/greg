2dcc11402a043a07b71d32e4c3ec6626852f3254
TaylorLearnsToCode <wtbeeson@gmail.com>
2023-05-14 20:43:57 -0400

    clean (generate treasure) break out jewels and gems for legibility

--- src/app/generate/services/generate-treasure/generate-lbb-treasure/generate-lbb-treasure.service.ts
+++ src/app/generate/services/generate-treasure/generate-lbb-treasure/generate-lbb-treasure.service.ts
@@ -18,6 +18,7 @@ import {
   isEmpty,
 } from '@shared/utilities/common-util/common.util';
 import { rollDice } from '@shared/utilities/dice-util/dice.util';
+import { LbbJewelsService } from './lbb-jewels/lbb-jewels.service';
 import { LbbSwordService } from './lbb-sword/lbb-sword.service';
 
 @Injectable({
@@ -27,40 +28,16 @@ export class GenerateLbbTreasureService
   extends AbstractTreasureGenerator
   implements TreasureGeneratorService
 {
-  /** Map of probability to jewelry value: from least valuable to most */
-  private readonly JEWELRY_VALUE_THRESHOLDS: Map<number, DiceRolled> = new Map([
-    [
-      20,
-      new DiceRolled({
-        multiplier: 100,
-        no: 3,
-      } as DiceRolled),
-    ],
-    [
-      80,
-      new DiceRolled({
-        multiplier: 1000,
-      } as DiceRolled),
-    ],
-    [
-      100,
-      new DiceRolled({
-        multiplier: 1000,
-        pips: 10,
-      } as DiceRolled),
-    ],
-  ]);
   /** Persisted data types supported by GREG */
   private readonly PERSISTENCE_TYPES = PERSISTENCE_TYPES;
 
   /** DecimalPipe for formatting */
   private decimalPipe: DecimalPipe = new DecimalPipe('en-US');
-  /** Response object built and returned by the generate jewelry function */
-  private jewelResult: ValueablesResult[];
 
   constructor(
     private dataService: DataManagerService,
     private gemsService: LbbGemsService,
+    private jewelsService: LbbJewelsService,
     private swordService: LbbSwordService
   ) {
     super();
@@ -81,20 +58,7 @@ export class GenerateLbbTreasureService
    * @param  {TreasureArticle} article
    */
   generateJewelry(article: TreasureArticle): ValueablesResult[] | null {
-    if (rollDice(article.diceRolled) <= article.chanceOf) {
-      this.jewelResult = [];
-      const jewelryCount: number = rollDice(article.quantity as DiceRolled);
-
-      let jewel: ValueablesResult;
-      for (let i = 0; i < jewelryCount; i++) {
-        jewel = this.generateJewel();
-        this.addOrIncrementJewel(jewel);
-      }
-
-      this.jewelResult.sort((a, b) => a.value - b.value);
-      return this.jewelResult;
-    }
-    return null;
+    return this.jewelsService.generateJewelry(article);
   }
 
   /**
@@ -223,23 +187,6 @@ export class GenerateLbbTreasureService
   }
 
   /**
-   * If no jewel of the present value is detected in the active result response, adds this
-   * to it. If at least one result is present already, increments the pre-existing set.
-   *
-   * @param  {ValueablesResult} jewel
-   */
-  private addOrIncrementJewel(jewel: ValueablesResult): void {
-    const jewelIndex: number = this.jewelResult.findIndex(
-      (j) => j.value === jewel.value
-    );
-    if (jewelIndex == -1) {
-      this.jewelResult.push(jewel);
-    } else {
-      this.jewelResult[jewelIndex].quantity += jewel.quantity;
-    }
-  }
-
-  /**
    * For any object with a "name" or, optionally, "quantity" property, converts said object
    * into a TreasureResult.
    *
@@ -255,35 +202,6 @@ export class GenerateLbbTreasureService
         : (namedItem.quantity as number);
     }
     return treasureResult;
-  }
-
-  /** Generates a new article of jewelry with calculated value */
-  private generateJewel(): ValueablesResult {
-    const jewel = new ValueablesResult({
-      value: this.generateJewelryValue(),
-      quantity: 1,
-    } as ValueablesResult);
-    jewel.name = `Jewelry (${jewel.value} ${jewel.denomination})`;
-    return jewel;
-  }
-
-  /** Generates a calculated jewelry value based on constant probability. */
-  private generateJewelryValue(): number {
-    const roll: number = rollDice(this.d100);
-    let returnValue: number = -1;
-    for (const key of this.JEWELRY_VALUE_THRESHOLDS.keys()) {
-      if (roll <= key) {
-        returnValue = rollDice(
-          this.JEWELRY_VALUE_THRESHOLDS.get(key) as DiceRolled
-        );
-        break;
-      }
-    }
-    if (returnValue === -1) {
-      throw new Error(`No jewelry value threshold found for roll ${roll}`);
-    } else {
-      return returnValue;
-    }
   }
 
   /**
