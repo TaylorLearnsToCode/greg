08142c179bac5180a6d675b46653131db1017147
TaylorLearnsToCode <clericswearemail@gmail.com>
2023-05-17 06:19:17 -0400

    feat (generate dungeon) add UI for selecting stocking list and system

--- src/app/generate/components/generate-dungeon/generate-dungeon.component.ts
+++ src/app/generate/components/generate-dungeon/generate-dungeon.component.ts
@@ -1,35 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SUPPORTED_SYSTEMS } from '@assets/supported-systems.config';
+import { DungeonResult } from '@generate/model/dungeon-result.model';
 import { GenerateDungeonService } from '@generate/services/generate-dungeon/generate-dungeon.service';
+import { ReferenceEntryTable } from '@shared/model/framework/reference-entry-table.model';
+import { DataManagerService } from '@shared/services/data-manager/data-manager.service';
+import { BehaviorSubject, Observable, map } from 'rxjs';
 
 @Component({
   selector: 'greg-generate-dungeon',
   templateUrl: './generate-dungeon.component.html',
   styleUrls: ['./generate-dungeon.component.scss'],
 })
-export class GenerateDungeonComponent {
+export class GenerateDungeonComponent implements OnInit {
+  dungeonResult$: Observable<any>;
+  monsterEncounterList$: Observable<ReferenceEntryTable[]>;
   noRooms: number = 0;
-  system: string = '';
+  stockingListRef: string = '';
   get supportedSystemKeys(): string[] {
     return Object.keys(SUPPORTED_SYSTEMS);
   }
   supportedSystem(key: string): string {
     return (SUPPORTED_SYSTEMS as any)[key];
   }
+  system: string = '';
 
-  constructor(private dungeonService: GenerateDungeonService) {}
+  private dungeonResultSource = new BehaviorSubject<DungeonResult | null>(null);
+
+  constructor(
+    private dataService: DataManagerService,
+    private dungeonService: GenerateDungeonService
+  ) {}
+
+  ngOnInit(): void {
+    this.dungeonResult$ = this.dungeonResultSource.asObservable();
+    this.monsterEncounterList$ = this.dataService.dataState$.pipe(
+      map((state) => state.monsterEncounterLists)
+    );
+  }
 
   clearForm(): void {
     alert('Implement this next!');
   }
 
   generateDungeon(): void {
-    alert('Implement this next!');
+    this.dungeonResultSource.next(
+      this.dungeonService.generateDungeon(this.noRooms, this.stockingListRef)
+    );
   }
 
   updateSystemSelection(event: Event): void {
     this.dungeonService.setSystemSelection(
       (event.target as HTMLSelectElement).value
     );
+  }
+
+  useList(list: ReferenceEntryTable): void {
+    this.stockingListRef = list.name;
+    if (list.system) {
+      this.setSystem(list.system);
+    }
+  }
+
+  private setSystem(newSystem: string): void {
+    this.system = newSystem;
+    this.dungeonService.setSystemSelection(newSystem);
   }
 }
---
+++ src/app/generate/model/dungeon-result.model.ts
@@ -0,0 +1,7 @@
+import { constructInstance } from '@shared/utilities/common-util/common.util';
+
+export class DungeonResult {
+  constructor(result?: any) {
+    constructInstance(this, result);
+  }
+}
