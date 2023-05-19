4dcec5cb1cd2401f88decf125030608a6db0fffb
TaylorLearnsToCode <clericswearemail@gmail.com>
2023-05-19 19:47:11 -0400

    feat (generate dungeon) Add BX generators

--- src/app/generate/model/abstract-inspiration-service.model.ts
+++ src/app/generate/model/abstract-inspiration-service.model.ts
@@ -1,19 +1,16 @@
 import { BoundedRange } from '@shared/model/utility/bounded-range.model';
 import { DiceRolled } from '@shared/model/utility/dice-rolled.model';
 import { isBetween } from '@shared/utilities/common-util/common.util';
-import { rollDice } from '@shared/utilities/dice-util/dice.util';
+import {
+  rollDice,
+  rollOnMappedList,
+} from '@shared/utilities/dice-util/dice.util';
 
 export abstract class AbstractInspirationService {
   protected readonly d6 = new DiceRolled();
 
   protected rollOnList(list: Map<number, string>): string {
-    const roll = rollDice(this.d6);
-    for (const key of list.keys()) {
-      if (key === roll) {
-        return ((' ' + list.get(key)) as string) + ' ';
-      }
-    }
-    throw new Error(`Unable to find list entry for result of ${roll}`);
+    return rollOnMappedList(list);
   }
 
   protected rollOnRange(
--- src/app/generate/services/generate-dungeon/generate-bx-dungeon/generate-bx-dungeon.service.ts
+++ src/app/generate/services/generate-dungeon/generate-bx-dungeon/generate-bx-dungeon.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { PERSISTENCE_TYPES } from '@assets/persistence-types.config';
 import { AbstractDungeonGenerator } from '@generate/model/abstract-dungeon-generator.model';
 import { DungeonGeneratorService } from '@generate/model/dungeon-generator-service.interface';
 import { DungeonResult } from '@generate/model/dungeon-result.model';
 import { DungeonRoomResult } from '@generate/model/dungeon-room-result.model';
+import { GenerateEncounterService } from '@generate/services/generate-encounter/generate-encounter.service';
+import { GenerateTreasureService } from '@generate/services/generate-treasure/generate-treasure.service';
+import { MonsterType } from '@shared/model/monster/monster-type.model';
+import { TreasureType } from '@shared/model/treasure/treasure-type.model';
+import { DiceRolled } from '@shared/model/utility/dice-rolled.model';
 import { DataManagerService } from '@shared/services/data-manager/data-manager.service';
+import { doesExist, isEmpty } from '@shared/utilities/common-util/common.util';
+import {
+  rollDice,
+  rollOnMappedList,
+} from '@shared/utilities/dice-util/dice.util';
 
 @Injectable({
   providedIn: 'root',
@@ -14,7 +25,49 @@ export class GenerateBxDungeonService
 {
   readonly TARGET_SYSTEM: string = 'BX';
 
-  constructor(private dataService: DataManagerService) {
+  private readonly d10 = new DiceRolled({ pips: 10 });
+  private readonly PERSISTENCE_TYPES = PERSISTENCE_TYPES;
+
+  private readonly SAMPLE_ROOM_TRAPS: Map<number, string> = new Map([
+    [1, 'Poison gas: Save vs. Poison or die'],
+    [2, 'Fog: Looks like Poison gas, but harmless'],
+    [3, "Pit: Id6 points of damage per 10' fallen"],
+    [
+      4,
+      'Ceiling Block falls: Save vs. Turn to Stone or take Id 10 points of damage',
+    ],
+    [5, 'Pendulum blade from ceiling: Id8 points of damage'],
+    [6, 'Chute: No damage, but slide to the next level down'],
+  ]);
+  private readonly SAMPLE_TREASURE_TRAPS: Map<number, string> = new Map([
+    [1, 'Poison needle: Save vs. Poison or die'],
+    [2, 'Spring-fired darts: 1-6 darts hit for 1-4 points of damage each'],
+    [3, 'Flash of light: Save vs. Spells or be blinded for Id8 turns'],
+    [4, 'Poison snake'],
+    [
+      5,
+      'Spray: Be sprayed with an unknown liquid that attracts Wandering Monsters; double chances for Id6 hours',
+    ],
+    [6, 'Illusion: Anything; often a monster'],
+  ]);
+  private readonly SAMPLE_SPECIAL_ROOMS: Map<number, string> = new Map([
+    [1, 'Moaning room or corridor'],
+    [2, 'Room turns or sinks while the door locks'],
+    [3, 'Illusionary stairs or corridor'],
+    [4, 'Shifting block to close off corridor'],
+    [5, 'Trap door to tunnels'],
+    [6, 'Alarm that summons special monster'],
+    [7, 'Talking statue'],
+    [8, 'Magic pool whose waters have a strange effect'],
+    [9, 'Magic gate to another part of the dungeon'],
+    [10, 'Flying weapons which attack only if disturbed'],
+  ]);
+
+  constructor(
+    private dataService: DataManagerService,
+    private encounterService: GenerateEncounterService,
+    private treasureService: GenerateTreasureService
+  ) {
     super();
   }
 
@@ -25,15 +78,11 @@ export class GenerateBxDungeonService
   ): DungeonResult {
     this.deriveStockingList(
       this.dataService,
-      this.TARGET_SYSTEM,
+      'LBB',
       dungeonLevel,
       stockingListRef
     );
-    this.deriveUnguardedTreasureType(
-      this.dataService,
-      this.TARGET_SYSTEM,
-      dungeonLevel
-    );
+    this.deriveUnguardedTreasureType(this.dataService, 'LBB', dungeonLevel);
     const dungeonResult = new DungeonResult();
     for (let i = 0; i < noRooms; i++) {
       dungeonResult.rooms.push(this.generateRoom());
@@ -41,9 +90,92 @@ export class GenerateBxDungeonService
     return dungeonResult;
   }
 
+  private addMonster(room: DungeonRoomResult): void {
+    room.monsters.push(
+      ...this.encounterService.generateEncounterFromList(this.stockingList)
+    );
+  }
+
+  private addSpecial(room: DungeonRoomResult): void {
+    room.specialFeature = `( Suggestion:${rollOnMappedList(
+      this.SAMPLE_SPECIAL_ROOMS,
+      this.d10
+    )})`;
+  }
+
+  private addTrap(room: DungeonRoomResult): void {
+    room.trap = `( Suggestion:${rollOnMappedList(this.SAMPLE_ROOM_TRAPS)})`;
+  }
+
+  private addTreasure(room: DungeonRoomResult): void {
+    if (room.hasMonster) {
+      if (!room.monsters.some((m) => !isEmpty(m.treasure))) {
+        let monster: MonsterType;
+        let monsterTreasureType: TreasureType;
+        for (const encounterResult of room.monsters) {
+          monster = this.dataService.retrieveReference<MonsterType>(
+            this.PERSISTENCE_TYPES.monsterType,
+            encounterResult.name
+          );
+          monsterTreasureType =
+            this.dataService.retrieveReference<TreasureType>(
+              this.PERSISTENCE_TYPES.treasureType,
+              monster?.treasureType,
+              'type'
+            );
+          if (doesExist(monsterTreasureType)) {
+            room.treasure.push(
+              ...this.treasureService.generateTreasure(monsterTreasureType)
+            );
+          } else {
+            console.warn(
+              `Error determining treasure for ${encounterResult.name}`
+            );
+          }
+          if (!isEmpty(room.treasure)) {
+            return;
+          }
+        }
+      }
+    }
+    room.treasure.push(
+      ...this.treasureService.generateTreasure(this.unguardedTreasureType)
+    );
+  }
+
   private generateRoom(): DungeonRoomResult {
     const room = new DungeonRoomResult();
-    // TO DO implement procedure
+    switch (rollDice(this.d6)) {
+      case 1:
+      case 2:
+        this.addMonster(room);
+        break;
+      case 3:
+        this.addTrap(room);
+        break;
+      case 4:
+        this.addSpecial(room);
+        break;
+      default:
+        break;
+    }
+    this.handleTreasure(room);
     return room;
+  }
+
+  private handleTreasure(room: DungeonRoomResult): void {
+    const roll = rollDice(this.d6);
+    if (room.hasMonster) {
+      if (roll < 4) {
+        this.addTreasure(room);
+      } else {
+        room.monsters.forEach((monster) => (monster.treasure = []));
+      }
+    } else if (room.hasMonster && roll < 4) {
+    } else if (room.hasTrap && roll < 3) {
+      this.addTreasure(room);
+    } else if (roll === 1) {
+      this.addTreasure(room);
+    }
   }
 }
