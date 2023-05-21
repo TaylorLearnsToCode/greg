3b5e78f9e6fd6bbcf5f54a929ca04f837f9a1d1c
TaylorLearnsToCode <clericswearemail@gmail.com>
2023-05-20 21:47:55 -0400

    feature (select system) add hint to welcome page

--- src/app/welcome/components/welcome-landing/welcome-landing.component.ts
+++ src/app/welcome/components/welcome-landing/welcome-landing.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PageDisplayMode } from '@shared/model/ui/page-display-mode.enum';
+import { AppComponentManagerService } from '@shared/services/app-component-manager/app-component-manager.service';
 
 @Component({
   selector: 'greg-welcome-landing',
@@ -8,4 +9,10 @@ import { PageDisplayMode } from '@shared/model/ui/page-display-mode.enum';
 })
 export class WelcomeLandingComponent {
   readonly pageDisplayMode = PageDisplayMode.SINGLE;
+
+  constructor(private appComponentService: AppComponentManagerService) {}
+
+  toggleConfig(target: string): void {
+    this.appComponentService.toggleCollapse(target);
+  }
 }
