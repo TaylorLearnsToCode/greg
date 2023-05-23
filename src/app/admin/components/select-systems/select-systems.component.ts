96460aa082ea2cc392d22b6e10fa94185aa31937
TaylorLearnsToCode <wtbeeson@gmail.com>
2023-05-22 22:37:13 -0400

    feat (select systems) Add BX initial treasure types to system select

--- src/app/admin/components/select-systems/select-systems.component.ts
+++ src/app/admin/components/select-systems/select-systems.component.ts
@@ -1,7 +1,7 @@
 import { SelectableSystem } from '@admin/model/selectable-system.model';
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { SUPPORTED_SYSTEMS } from '@assets/app-configs/supported-systems.config';
+import { bx } from '@assets/system-configs/system-bx';
 import { lbb } from '@assets/system-configs/system-lbb';
 import { DataManagerService } from '@shared/services/data-manager/data-manager.service';
 import { throwError } from '@shared/utilities/framework-util/framework.util';
@@ -16,10 +16,7 @@ export class SelectSystemsComponent {
 
   private readonly SUPPORTED_SYSTEMS = SUPPORTED_SYSTEMS;
 
-  constructor(
-    private dataService: DataManagerService,
-    private httpClient: HttpClient
-  ) {
+  constructor(private dataService: DataManagerService) {
     this.supportedSystems = Object.keys(this.SUPPORTED_SYSTEMS).map(
       (key) =>
         new SelectableSystem({
@@ -30,19 +27,18 @@ export class SelectSystemsComponent {
   }
 
   loadSystem(system: SelectableSystem, replace?: boolean): void {
-    if (system.futureScope) {
-      alert(`System ${system.key} is not yet avaialble for prime time.`);
-      return;
-    }
-
     let selectedSystem;
     switch (system.key) {
+      case 'BX':
+        selectedSystem = bx;
+        break;
       case 'LBB':
         selectedSystem = lbb;
         break;
       default:
-        throwError(`System ${system.key} not found`);
+        throwError(`System ${system.key} is not yet avaialble for prime time.`);
     }
     this.dataService.persist('master', selectedSystem);
+    alert(`${system.key} successfully loaded.`);
   }
 }
