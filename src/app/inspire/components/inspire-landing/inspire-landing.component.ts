af05351394c51296c8518336ff7301d330171308
TaylorLearnsToCode <clericswearemail@gmail.com>
2023-05-24 06:07:10 -0400

    feat (inspire) add holmes homage

--- src/app/inspire/components/inspire-landing/inspire-landing.component.ts
+++ src/app/inspire/components/inspire-landing/inspire-landing.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { PageDisplayMode } from '@shared/model/ui/page-display-mode.enum';
 
 @Component({
   selector: 'greg-inspire-landing',
   templateUrl: './inspire-landing.component.html',
-  styleUrls: ['./inspire-landing.component.scss']
+  styleUrls: ['./inspire-landing.component.scss'],
 })
 export class InspireLandingComponent {
-
+  pageDisplayMode: PageDisplayMode = PageDisplayMode.SINGLE;
 }
