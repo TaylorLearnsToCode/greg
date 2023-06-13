7280fe16f2725eeaae6c0434e735fd16d7c536c7
TaylorLearnsToCode <clericswearemail@gmail.com>
2023-06-13 18:14:15 -0400

    clean (generate encounter) reduce code duplication via inheritance

--- src/app/generate/model/abstract-encounter-generator.model.ts
+++ src/app/generate/model/abstract-encounter-generator.model.ts
@@ -1,11 +1,161 @@
 import { PERSISTENCE_TYPES } from '@assets/app-configs/persistence-types.config';
+import {
+  squashEncounterResults,
+  squashEncounterTreasure,
+} from '@generate/utilities/encounter-util/encounter.util';
+import { ReferenceEntryTable } from '@shared/model/framework/reference-entry-table.model';
+import { ReferenceEntry } from '@shared/model/framework/reference-entry.model';
+import { MonsterConsort } from '@shared/model/monster/monster-consort.model';
+import { MonsterRetinue } from '@shared/model/monster/monster-retinue.model';
+import { MonsterType } from '@shared/model/monster/monster-type.model';
+import { BoundedRange } from '@shared/model/utility/bounded-range.model';
 import { DiceRolled } from '@shared/model/utility/dice-rolled.model';
+import {
+  doesExist,
+  isBetween,
+} from '@shared/utilities/common-util/common.util';
+import { rollDice } from '@shared/utilities/dice-util/dice.util';
 import { EncounterResult } from './encounter-result.model';
 
 export abstract class AbstractEncounterGenerator {
   protected readonly d100 = new DiceRolled({ pips: 100 });
-
   protected readonly PERSISTENCE_TYPES = PERSISTENCE_TYPES;
 
   protected result: EncounterResult[] = [];
+
+  abstract handleTreasure(monster: MonsterType, result: EncounterResult): void;
+  abstract populateEncounterResult(
+    result: EncounterResult,
+    monster: MonsterType
+  ): void;
+  abstract retrieveEncounterReferenceTable(name: string): ReferenceEntryTable;
+  abstract retrieveMonsterReference(name: string): MonsterType;
+
+  generateEncounterFromList(
+    list: ReferenceEntryTable,
+    noSquash?: boolean
+  ): EncounterResult[] {
+    this.result = [];
+    const listRoll: number = rollDice(list.diceToRoll);
+    for (let entry of list.entries) {
+      if (isBetween(listRoll, entry.chanceOf)) {
+        this.result.push(this.generateReferenceEncounter(entry));
+      }
+    }
+    if (!noSquash) {
+      this.squashResult();
+    }
+    return this.result;
+  }
+
+  private followingHandledAsCustom(
+    following: MonsterConsort | MonsterRetinue
+  ): void {
+    this.result.push(
+      new EncounterResult({
+        name: following.name,
+        quantity: rollDice(following.quantity),
+      } as EncounterResult)
+    );
+  }
+
+  private followingHandledAsReference(
+    following: MonsterConsort | MonsterRetinue
+  ): boolean {
+    const consortReference: MonsterType = this.retrieveMonsterReference(
+      following.name
+    );
+    if (!doesExist(consortReference)) {
+      return false;
+    }
+    this.result.push(
+      this.generateReferenceEncounter(
+        new ReferenceEntry({
+          chanceOf: new BoundedRange({ low: 1, high: 100 }),
+          persistenceType: this.PERSISTENCE_TYPES.monsterType,
+          reference: consortReference.name,
+        })
+      )
+    );
+    return true;
+  }
+
+  private followingHandledAsTable(
+    following: MonsterConsort | MonsterRetinue
+  ): boolean {
+    const tableReference: ReferenceEntryTable =
+      this.retrieveEncounterReferenceTable(following.name);
+    if (!doesExist(tableReference)) {
+      return false;
+    }
+    const currentResult: EncounterResult[] = this.result;
+    currentResult.push(...this.generateEncounterFromList(tableReference, true));
+    this.result = currentResult;
+    return true;
+  }
+
+  private generateReferenceEncounter(ref: ReferenceEntry): EncounterResult {
+    let result = new EncounterResult();
+
+    const monster: MonsterType = this.retrieveMonsterReference(ref.reference);
+    if (doesExist(monster)) {
+      this.populateEncounterResult(result, monster);
+      this.handleTreasure(monster, result);
+      this.handleConsorts(monster, result);
+      this.handleRetinue(monster, result);
+    } else {
+      const monsterList: ReferenceEntryTable =
+        this.retrieveEncounterReferenceTable(ref.reference);
+      result = this.generateEncounterFromList(monsterList, true)[0];
+    }
+
+    return result;
+  }
+
+  private handleConsorts(monster: MonsterType, result: EncounterResult): void {
+    if (monster.consorts) {
+      let numberConsorting: number;
+      for (const consort of monster.consorts) {
+        if (consort.every === 0) {
+          numberConsorting = 1;
+        } else {
+          numberConsorting = Math.floor(result.quantity / consort.every);
+        }
+        for (let i = 0; i < numberConsorting; i++) {
+          if (rollDice(this.d100) <= consort.pctChance) {
+            if (!this.followingHandledAsTable(consort)) {
+              if (!this.followingHandledAsReference(consort)) {
+                this.followingHandledAsCustom(consort);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  private handleRetinue(monster: MonsterType, result: EncounterResult) {
+    if (monster.retinue) {
+      let chanceOf: number;
+      for (const retinue of monster.retinue) {
+        chanceOf =
+          Math.floor(result.quantity / retinue.each) * retinue.incChance;
+        if (rollDice(this.d100) <= chanceOf) {
+          if (!this.followingHandledAsTable(retinue)) {
+            if (!this.followingHandledAsReference(retinue)) {
+              this.followingHandledAsCustom(retinue);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  private squashResult(): void {
+    const squashedResult: EncounterResult[] = [];
+    squashEncounterResults(this.result, squashedResult);
+    squashEncounterTreasure(squashedResult);
+    squashedResult.reverse();
+    this.result = squashedResult;
+  }
 }
--- src/app/generate/services/generate-encounter/generate-bx-encounter/generate-bx-encounter.service.ts
+++ src/app/generate/services/generate-encounter/generate-bx-encounter/generate-bx-encounter.service.ts
@@ -2,23 +2,13 @@ import { Injectable } from '@angular/core';
 import { AbstractEncounterGenerator } from '@generate/model/abstract-encounter-generator.model';
 import { EncounterGeneratorService } from '@generate/model/encounter-generator-service.interface';
 import { EncounterResult } from '@generate/model/encounter-result.model';
-import { TreasureResult } from '@generate/model/treasure-result.model';
 import { GenerateBxTreasureService } from '@generate/services/generate-treasure/generate-bx-treasure/generate-bx-treasure.service';
 import { ReferenceEntryTable } from '@shared/model/framework/reference-entry-table.model';
-import { ReferenceEntry } from '@shared/model/framework/reference-entry.model';
-import { MonsterConsort } from '@shared/model/monster/monster-consort.model';
-import { MonsterRetinue } from '@shared/model/monster/monster-retinue.model';
 import { MonsterType } from '@shared/model/monster/monster-type.model';
 import { TreasureArticle } from '@shared/model/treasure/treasure-article.model';
 import { TreasureType } from '@shared/model/treasure/treasure-type.model';
-import { BoundedRange } from '@shared/model/utility/bounded-range.model';
 import { DataManagerService } from '@shared/services/data-manager/data-manager.service';
-import {
-  doesExist,
-  findIndexMatchingAllKeys,
-  isBetween,
-  isEmpty,
-} from '@shared/utilities/common-util/common.util';
+import { doesExist, isEmpty } from '@shared/utilities/common-util/common.util';
 import { rollDice } from '@shared/utilities/dice-util/dice.util';
 
 @Injectable({
@@ -35,139 +25,7 @@ export class GenerateBxEncounterService
     super();
   }
 
-  generateEncounterFromList(
-    list: ReferenceEntryTable,
-    noSquash?: boolean
-  ): EncounterResult[] {
-    this.result = [];
-    const listRoll: number = rollDice(list.diceToRoll);
-    for (let entry of list.entries) {
-      if (isBetween(listRoll, entry.chanceOf)) {
-        this.result.push(this.generateReferenceEncounter(entry));
-      }
-    }
-    if (!noSquash) {
-      this.squashResult();
-    }
-    return this.result;
-  }
-
-  private followingHandledAsCustom(
-    following: MonsterConsort | MonsterRetinue
-  ): void {
-    this.result.push(
-      new EncounterResult({
-        name: following.name,
-        quantity: rollDice(following.quantity),
-      } as EncounterResult)
-    );
-  }
-
-  private followingHandledAsReference(
-    following: MonsterConsort | MonsterRetinue
-  ): boolean {
-    const consortReference: MonsterType = this.dataService.retrieveReference(
-      following.name,
-      this.PERSISTENCE_TYPES.monsterType
-    );
-    if (!doesExist(consortReference)) {
-      return false;
-    }
-    this.result.push(
-      this.generateReferenceEncounter(
-        new ReferenceEntry({
-          chanceOf: new BoundedRange({ low: 1, high: 100 }),
-          persistenceType: this.PERSISTENCE_TYPES.monsterType,
-          reference: consortReference.name,
-        })
-      )
-    );
-    return true;
-  }
-
-  private followingHandledAsTable(
-    following: MonsterConsort | MonsterRetinue
-  ): boolean {
-    const tableReference: ReferenceEntryTable =
-      this.dataService.retrieveReference(
-        following.name,
-        this.PERSISTENCE_TYPES.monsterEncounterList
-      );
-    if (!doesExist(tableReference)) {
-      return false;
-    }
-    const currentResult: EncounterResult[] = this.result;
-    currentResult.push(...this.generateEncounterFromList(tableReference, true));
-    this.result = currentResult;
-    return true;
-  }
-
-  private generateReferenceEncounter(ref: ReferenceEntry): EncounterResult {
-    let result = new EncounterResult();
-
-    const monster: MonsterType = this.dataService.retrieveReference(
-      ref.reference,
-      this.PERSISTENCE_TYPES.monsterType
-    );
-    if (doesExist(monster)) {
-      result.name = monster.name;
-      result.quantity = rollDice(monster.quantity);
-
-      this.handleTreasure(monster, result);
-      this.handleConsorts(monster, result);
-      this.handleRetinue(monster, result);
-    } else {
-      const monsterList: ReferenceEntryTable =
-        this.dataService.retrieveReference(
-          ref.reference,
-          this.PERSISTENCE_TYPES.monsterEncounterList
-        );
-      result = this.generateEncounterFromList(monsterList, true)[0];
-    }
-
-    return result;
-  }
-
-  private handleConsorts(monster: MonsterType, result: EncounterResult) {
-    if (monster.consorts) {
-      let numberConsorting: number;
-      for (const consort of monster.consorts) {
-        if (consort.every === 0) {
-          numberConsorting = 1;
-        } else {
-          numberConsorting = Math.floor(result.quantity / consort.every);
-        }
-        for (let i = 0; i < numberConsorting; i++) {
-          if (rollDice(this.d100) <= consort.pctChance) {
-            if (!this.followingHandledAsTable(consort)) {
-              if (!this.followingHandledAsReference(consort)) {
-                this.followingHandledAsCustom(consort);
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-
-  private handleRetinue(monster: MonsterType, result: EncounterResult) {
-    if (monster.retinue) {
-      let chanceOf: number;
-      for (const retinue of monster.retinue) {
-        chanceOf =
-          Math.floor(result.quantity / retinue.each) * retinue.incChance;
-        if (rollDice(this.d100) <= chanceOf) {
-          if (!this.followingHandledAsTable(retinue)) {
-            if (!this.followingHandledAsReference(retinue)) {
-              this.followingHandledAsCustom(retinue);
-            }
-          }
-        }
-      }
-    }
-  }
-
-  private handleTreasure(monster: MonsterType, result: EncounterResult) {
+  handleTreasure(monster: MonsterType, result: EncounterResult) {
     result.treasureType = monster.treasureType;
     if (result.isLair && !isEmpty(monster.treasureType)) {
       const type: TreasureType = this.dataService.retrieveReference(
@@ -210,48 +68,22 @@ export class GenerateBxEncounterService
     }
   }
 
-  private squashResult(): void {
-    const squashedResult: EncounterResult[] = [];
-    let resultIndex: number;
-    for (const result of this.result) {
-      resultIndex = findIndexMatchingAllKeys<EncounterResult>(
-        result,
-        squashedResult,
-        ['name']
-      );
-      if (resultIndex === -1) {
-        squashedResult.push(new EncounterResult(result));
-      } else {
-        squashedResult[resultIndex].quantity += result.quantity;
-        squashedResult[resultIndex].treasure.push(...result.treasure);
-        if (result.isLair && !squashedResult[resultIndex].isLair) {
-          squashedResult[resultIndex].isLair = true;
-        }
-      }
-    }
+  populateEncounterResult(result: EncounterResult, monster: MonsterType): void {
+    result.name = monster.name;
+    result.quantity = rollDice(monster.quantity);
+  }
 
-    let treasureIndex: number;
-    for (const result of squashedResult) {
-      if (!isEmpty(result.treasure)) {
-        const squashedTreasure: TreasureResult[] = [];
-        for (const treasure of result.treasure) {
-          treasureIndex = findIndexMatchingAllKeys<TreasureResult>(
-            treasure,
-            squashedTreasure,
-            ['name']
-          );
-          if (treasureIndex === -1) {
-            squashedTreasure.push(new TreasureResult(treasure));
-          } else {
-            squashedTreasure[treasureIndex].quantity += treasure.quantity;
-          }
-        }
-        result.treasure = squashedTreasure;
-      }
-    }
+  retrieveEncounterReferenceTable(name: string): ReferenceEntryTable {
+    return this.dataService.retrieveReference(
+      name,
+      this.PERSISTENCE_TYPES.monsterEncounterList
+    );
+  }
 
-    squashedResult.reverse();
-
-    this.result = squashedResult;
+  retrieveMonsterReference(name: string): MonsterType {
+    return this.dataService.retrieveReference(
+      name,
+      this.PERSISTENCE_TYPES.monsterType
+    );
   }
 }
