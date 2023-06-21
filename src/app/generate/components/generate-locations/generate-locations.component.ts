34781500c16cd74baa7057d0390ee319b468b737
TaylorLearnsToCode <clericswearemail@gmail.com>
2023-06-21 16:56:08 -0400

    clean (generate location) clean up typescript

--- src/app/generate/components/generate-locations/generate-locations.component.ts
+++ src/app/generate/components/generate-locations/generate-locations.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BoundedRange } from '@shared/model/utility/bounded-range.model';
 
 @Component({
@@ -6,17 +6,10 @@ import { BoundedRange } from '@shared/model/utility/bounded-range.model';
   templateUrl: './generate-locations.component.html',
   styleUrls: ['./generate-locations.component.scss']
 })
-export class GenerateLocationsComponent implements OnInit, AfterViewInit {
+export class GenerateLocationsComponent implements OnInit {
   classMatrix: Map<BoundedRange, string> = new Map();
   width: number = 11;
   height: number = 9;
-
-  get colNos(): number[] {
-    return Array<number>(this.width).fill(1).map((element, index) => element + index);
-  }
-  get rowNos(): number[] {
-    return Array<number>(this.height).fill(1).map((element, index) => element + index);
-  }
 
   get centerCol(): number {
     return Math.ceil(this.width / 2);
@@ -24,59 +17,15 @@ export class GenerateLocationsComponent implements OnInit, AfterViewInit {
   get centerRow(): number {
     return Math.ceil(this.height / 2);
   }
+  get colNos(): number[] {
+    return Array<number>(this.width).fill(1).map((element, index) => element + index);
+  }
+  get rowNos(): number[] {
+    return Array<number>(this.height).fill(1).map((element, index) => element + index);
+  }
 
   ngOnInit(): void {
     this.deriveClassMatrix();
-  }
-
-  ngAfterViewInit(): void {
-    console.warn(this.classMatrix);
-  }
-
-
-  deriveClassMatrix(): void {
-    this.classMatrix.clear();
-
-    let key: BoundedRange;
-    let value: string;
-    let stepsFromCenterColumn: number;
-    let totalCellsHighlighted: number;
-    let cellCount: number;
-    let rowRadius: number;
-    for (const colNo of this.colNos) {
-      stepsFromCenterColumn = Math.abs(this.centerCol - colNo);
-      switch (stepsFromCenterColumn) {
-        case 0:
-        case 1:
-        case 2:
-        case 3:
-          totalCellsHighlighted = 7 - stepsFromCenterColumn;
-          break;
-        case 4:
-          totalCellsHighlighted = 1;
-          break;
-        default:
-          totalCellsHighlighted = 0;
-      }
-
-      cellCount = 0;
-      for (const rowNo of this.rowNos) {
-        key = new BoundedRange({ low: colNo, high: rowNo });
-
-        rowRadius = Math.floor(totalCellsHighlighted / 2);
-        if (
-          rowRadius >= Math.abs(this.centerRow - rowNo) &&
-          cellCount < totalCellsHighlighted
-        ) {
-          value = 'highlight-cell';
-          cellCount++;
-        } else {
-          value = '';
-        }
-
-        this.classMatrix.set(key, value);
-      }
-    }
   }
 
   getClassByCoords(col: number, row: number): string[] {
@@ -88,5 +37,54 @@ export class GenerateLocationsComponent implements OnInit, AfterViewInit {
       }
     }
     return returnValue;
+  }
+
+  private calculateTotalCellsHighlighted(colNo: number): number {
+    const stepsFromCenterColumn = Math.abs(this.centerCol - colNo);
+    let totalCellsHighlighted: number;
+    switch (stepsFromCenterColumn) {
+      case 0:
+      case 1:
+      case 2:
+      case 3:
+        totalCellsHighlighted = 7 - stepsFromCenterColumn;
+        break;
+      case 4:
+        totalCellsHighlighted = 1;
+        break;
+      default:
+        totalCellsHighlighted = 0;
+    } return totalCellsHighlighted;
+  }
+
+  private deriveClassMatrix(): void {
+    this.classMatrix.clear();
+
+    let totalCellsHighlighted: number;
+    let cellCount: number;
+    for (const colNo of this.colNos) {
+      totalCellsHighlighted = this.calculateTotalCellsHighlighted(colNo);
+      cellCount = 0;
+      for (const rowNo of this.rowNos) {
+        cellCount += this.insertMatrixRecord(colNo, rowNo, totalCellsHighlighted, cellCount);
+      }
+    }
+  }
+
+  private insertMatrixRecord(colNo: number, rowNo: number, totalCellsHighlighted: number, cellCount: number): number {
+    const rowRadius = Math.floor(totalCellsHighlighted / 2);
+
+    let retVal = 0;
+    if (
+      rowRadius >= Math.abs(this.centerRow - rowNo) &&
+      cellCount < totalCellsHighlighted
+    ) {
+      retVal++;
+      this.classMatrix.set(new BoundedRange({ low: colNo, high: rowNo }), 'highlight-cell');
+    } else {
+      this.classMatrix.set(new BoundedRange({ low: colNo, high: rowNo }), '');
+    }
+
+    return retVal;
   }
 }
